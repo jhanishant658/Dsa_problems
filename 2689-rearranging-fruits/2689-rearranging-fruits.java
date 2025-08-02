de1f0307d795e5a03daee60ca01bf641c3bc986d
Nishant jha <jhanishant658@gmail.com>
2025-08-02 11:44:06 +0530

    Time: 41 ms (92.96%), Space: 64.1 MB (74.65%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.java
+++ 2689-rearranging-fruits/2689-rearranging-fruits.java
@@ -1,23 +1,35 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
         HashMap<Integer,Integer> map = new HashMap<>() ; 
-        for(int num:basket1)map.put(num,map.getOrDefault(num,0)+1);
-         for(int num:basket2)map.put(num,map.getOrDefault(num,0)-1);
-         List<Integer> excess = new ArrayList<>() ; 
-         int min = Integer.MAX_VALUE ; 
-         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int val = entry.getKey() ;
+        
+        for(int num : basket1)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+        for(int num : basket2)
+            map.put(num, map.getOrDefault(num, 0) - 1);
+        
+        List<Integer> excess = new ArrayList<>() ; 
+        int min = Integer.MAX_VALUE ; 
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int val = entry.getKey();
             int diff = entry.getValue();
-            if(diff%2!=0)return - 1 ; 
-            for(int i = 0 ; i<Math.abs(diff)/2 ;i++)excess.add(val) ;
-            min =Math.min(min , val);
-         }
-         //calculate cost 
-         Collections.sort(excess) ; 
-         long cost = 0 ;
-         for(int i = 0 ;i<excess.size()/2 ;i++){
-            cost+= Math.min(excess.get(i) , 2*min);
-         }
+            
+            if(diff % 2 != 0) return -1;
+            
+            for(int i = 0 ; i < Math.abs(diff) / 2 ; i++)
+                excess.add(val);
+            
+            min = Math.min(min, val);
+        }
+
+        Collections.sort(excess);  // ⭐ important to ensure smallest first
+        
+        long cost = 0; // ✅ long instead of int
+        
+        for(int i = 0 ; i < excess.size() / 2 ; i++) {
+            cost += Math.min(excess.get(i), 2 * min);
+        }
         
         return cost;
     }
