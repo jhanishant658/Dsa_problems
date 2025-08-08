fe219465b053f2ed74b7eede607cad3e205a6c57
Nishant jha <jhanishant658@gmail.com>
2025-08-08 12:57:25 +0530

    Time: 15 ms (38.92%), Space: 44.9 MB (52.69%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.java
+++ 0826-soup-servings/0826-soup-servings.java
@@ -5,21 +5,14 @@ public class Solution {
     Map<String, Double> memo = new HashMap<>();
 
     public double ans(int a, int b) {
-        if (a <= 0 && b <= 0) return 0.5;
-        if (a <= 0) return 1.0;
-        if (b <= 0) return 0.0;
-
-        String key = a + "," + b;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        double res = 0.25 * (
-            ans(a - 100, b) +
-            ans(a - 75, b - 25) +
-            ans(a - 50, b - 50) +
-            ans(a - 25, b - 75)
-        );
-        memo.put(key, res);
-        return res;
+        if(a<=0&b<=0)return 0.5 ;
+        if(a<=0) return 1 ;
+        if(b<=0)return 0 ; 
+        String key =a+","+b ;
+        if(memo.containsKey(key))return memo.get(key);
+         double res  = 0.25*(ans(a-100 ,b)+ans(a-75 , b-25)+ans(a-50 , b-50)+ans(a-25 , b-75)) ; 
+         memo.put(key , res) ; 
+         return res ; 
     }
 
     public double soupServings(int n) {
