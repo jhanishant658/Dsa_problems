5ec7202d656f731614189b33780d2ec7e454e0a0
Nishant jha <jhanishant658@gmail.com>
2025-09-25 14:10:06 +0530

    Time: 1 ms (20.88%), Space: 42 MB (47.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,7 +4,7 @@ class Solution {
     for(int i = 0 ; i<nums.length ; i++){
         map.put(nums[i] , map.getOrDefault(nums[i] ,0)+1);
     } 
-    System.out.println(map); 
+    
     int idx = 0 ; 
     for(int i = 0 ;i<=2 ; i++){
        int count = map.getOrDefault(i , 0);
