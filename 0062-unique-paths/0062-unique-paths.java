8525a30fec3197347cca41cc248c0771c526c958
Nishant jha <jhanishant658@gmail.com>
2025-09-27 18:54:29 +0530

    Time: 0 ms (100%), Space: 40.5 MB (62.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,16 +4,13 @@ class Solution {
       grid[0][0] = 1 ; 
      for(int i = 1 ; i<n ; i++){
         grid[0][i] = 1 ; 
-        System.out.println("minimum path for " +0 +"and"+i+"is " + grid[0][i]);
      }
      for(int i = 1 ; i<m ;i++){
         grid[i][0] = 1 ; 
-         System.out.println("minimum path for " +i +"and"+0+"is " + grid[i][0]);
      }
      for(int i=1 ; i<m ; i++){
         for(int j= 1 ; j<n ; j++){
-            grid[i][j] = grid[i-1][j] +grid[i][j-1]  ;
-             System.out.println("minimum path for " +i +"and"+j+"is " + grid[i][j]); 
+            grid[i][j] = grid[i-1][j] +grid[i][j-1]  ; 
         }
      }
      return grid[m-1][n-1] ; 
