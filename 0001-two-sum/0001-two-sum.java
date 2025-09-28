03bb11fcf364393439c254c52464233dba9f8797
Nishant jha <jhanishant658@gmail.com>
2025-09-28 15:52:32 +0530

    Time: 9 ms (50.08%), Space: 47 MB (5.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int[][] copy = new int[nums.length][2];  // Each element: [index, value]
-
-        for (int i = 0; i < nums.length; i++) {
-            copy[i][0] = i;        // store index
-            copy[i][1] = nums[i];  // store value
+       HashMap<Integer ,List<Integer>> map = new HashMap<>() ;
+       for(int i = 0 ;i<nums.length ; i++){
+           map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+       }
+       Arrays.sort(nums);
+       int i = 0 , j = nums.length-1 ;
+       while(i<j){
+        if(nums[i]+nums[j]==target){
+            if(nums[i]==nums[j]) return new int[]{map.get(nums[i]).get(0) ,map.get(nums[i]).get(1)};
+            return new int[]{map.get(nums[i]).get(0) , map.get(nums[j]).get(0)} ; 
         }
-
-        // Step 1: Sort by value (ascending)
-        Arrays.sort(copy, (a, b) -> Integer.compare(a[1], b[1]));
-        int i = 0 ,j= nums.length-1 ; 
-        while(i<j){
-            if(copy[i][1]+copy[j][1]==target) return new int[]{copy[i][0] , copy[j][0]};
-            else if(copy[i][1]+copy[j][1]<target)i++;
-            else j-- ;
-        }
-        return new int[]{-1,-1};
+        if(nums[i]+nums[j]<target) i++ ; 
+        else j-- ;
+       }
+       return new int[]{-1 ,-1};
     }
 }
