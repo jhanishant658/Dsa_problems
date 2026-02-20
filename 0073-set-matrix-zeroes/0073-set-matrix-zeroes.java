4e6c2680eb288bb5525bdb92932f1178a0f44d77
Nishant jha <jhanishant658@gmail.com>
2026-02-20 16:18:10 +0530

    Time: 1 ms (60.21%), Space: 47.7 MB (13.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,36 +1,28 @@
 class Solution {
-
-    public void setZeroes(int[][] matrix) {
-        boolean [][] visited = new boolean [matrix.length][matrix[0].length];
-     for(int i = 0 ;i<matrix.length ;i++){
-        for(int j = 0 ;j<matrix[0].length ;j++){
-            if(!visited[i][j]&&matrix[i][j]==0){
-                // go downward 
-              for(int k = i ;k<matrix.length ;k++){
-                 if(matrix[k][j]!=0){ matrix[k][j] = 0 ;
-                     visited[k][j]=true ; 
-                 }
-              }
-              // go upward 
-              for(int k = i ;k>=0 ;k--){
-                 if(matrix[k][j]!=0){ matrix[k][j] = 0 ;
-                     visited[k][j]=true ; 
-                 }
-              }
-              // go right 
-              for(int k = j ;k<matrix[0].length ;k++){
-                 if(matrix[i][k]!=0){ matrix[i][k] = 0 ;
-                     visited[i][k]=true ; 
-                 }
-              }
-              // go left 
-              for(int k = j ;k>=0 ;k--){
-                 if(matrix[i][k]!=0){ matrix[i][k] = 0 ;
-                     visited[i][k]=true ; 
-                 }
-              }
-              }
+    public void setZero(int[][] matrix , int row , int col , boolean[][]visited){
+        for(int i = 0 ; i<matrix[0].length ; i++){
+            if(matrix[row][i]!=0){
+                visited[row][i] = true ; 
+                matrix[row][i] = 0 ; 
             }
         }
-     }
- }
+        for(int i = 0 ; i<matrix.length ; i++){
+            if(matrix[i][col]!=0){
+                visited[i][col] = true ; 
+                matrix[i][col] = 0 ; 
+            }
+        }
+    }
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length , n = matrix[0].length ; 
+        boolean [][]visited = new boolean[m][n] ; 
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(!visited[i][j]&&matrix[i][j]==0){
+                    visited[i][j]= true ; 
+                    setZero(matrix , i , j , visited) ; 
+                }
+            }
+        }
+    }
+}
