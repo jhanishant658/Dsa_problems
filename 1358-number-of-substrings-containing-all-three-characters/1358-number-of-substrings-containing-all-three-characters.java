17e6008ca67b8d84defb995723b0769cc21f99a8
Nishant jha <jhanishant658@gmail.com>
2026-06-30 10:28:04 +0530

    Time: 17 ms (27.99%), Space: 46.2 MB (62.27%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,25 +1,14 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int ans = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            // 1. Add current character first
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            
-            // 2. Shrink window from the left as long as it's valid
-            while (map.containsKey('a') && map.containsKey('b') && map.containsKey('c')) {
-                // All substrings from index 'i' to the end of the string are valid
-                ans += s.length() - i; 
-                
-                char leftChar = s.charAt(left);
-                if (map.get(leftChar) == 1) {
-                    map.remove(leftChar);
-                } else {
-                    map.put(leftChar, map.get(leftChar) - 1); // Fixed subtraction
-                }
-                left++;
+        int[] arr = new int[3];
+        int left = 0 ; 
+        for(int i = 0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            while(arr[0]!=0&&arr[1]!=0&&arr[2]!=0){
+                 ans += s.length() - i ; 
+                 arr[s.charAt(left)-'a']--;
+                 left++;
             }
         }
         return ans;
